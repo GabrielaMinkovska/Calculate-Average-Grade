e8f3f865f3311f9dea75cb013a1897be1a47036b
Gabi <Gabi@DESKTOP-NRGQFIP>
2024-03-01 12:04:17 +0200

    Add project files.

---
+++ T01_Calculate Average Grade/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace T01_Calculate_Average_Grade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int studentCount = int.Parse(Console.ReadLine());
+
+            double grades = 0;
+
+            for (int i = 1; i <= studentCount; i++)
+            {
+
+                double grade = double.Parse(Console.ReadLine());
+                grades += grade;
+            }
+
+            double averageGrade = grades / studentCount;
+
+            Console.WriteLine($"{averageGrade:F2}");
+        }
+    }
+}
